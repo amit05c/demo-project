4ec918f06ef6ff29a8d360771b6fe5307fc61bf2
amit05c <amitghosh0595c@gmail.com>
2025-08-16 16:56:35 +0530

    testing2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Testing</h1>
+        <h1>Testinh2</h1>
         <Image
           className="dark:invert"
           src="/next.svg"
